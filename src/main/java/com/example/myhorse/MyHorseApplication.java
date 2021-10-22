b6e3d1cc53be6bafb0a6a06c6dbc10342f2dd3d7
Kinga <kinga2233@onet.pl>
2021-10-22 13:07:37 +0200

    Initial commit

---
+++ src/main/java/com/example/myhorse/MyHorseApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHorseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHorseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myhorse/MyHorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhorse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHorseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
